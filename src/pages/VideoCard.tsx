b15c8de1df4ab97e305587dfa1d0f9550cca3216
wondonghwi <ehdehd1289@naver.com>
2024-02-23 00:55:53 +0900

    feat: videoCard UI 작업

--- src/pages/VideoCard.tsx
+++ src/pages/VideoCard.tsx
@@ -1,9 +1,27 @@
 import { VideoItem } from '../interfaces/popular';
+import { formatAgo } from '../util/date';
 
 interface VideoCardProps {
   video: VideoItem;
 }
 
 export default function VideoCard({ video }: VideoCardProps) {
-  return <li>{video.snippet.title}</li>;
+  const { title, thumbnails, channelTitle, publishedAt } = video.snippet;
+
+  return (
+    <>
+      <li>
+        <img
+          className='w-full'
+          src={thumbnails.medium.url}
+          alt={title}
+        />
+        <div>
+          <p className='font-semibold my-2 line-clamp-2'>{title}</p>
+          <p>{channelTitle}</p>
+          <p>{formatAgo(publishedAt, 'ko')}</p>
+        </div>
+      </li>
+    </>
+  );
 }
